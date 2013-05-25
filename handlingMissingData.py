3c4c8251ec72b6e3233a3606198b61634ccf8354
inuishan <ishanjain1991@gmail.com>
2013-05-25 20:41:49 +0530

    removes files without folders

--- handlingMissingData.py
+++
@@ -1,5 +0,0 @@
-from pandas import Series, DataFrame
-import pandas as pd
-import numpy as np
-ser = Series(['a','b',np.nan,'c'])
-print ser.isnull()
--- rank.py
+++
@@ -1,6 +0,0 @@
-from pandas import Series, DataFrame
-import pandas as pd
-import numpy as np
-obj = Series([1,-5,2,2,4])
-print obj.rank()
-print obj.rank(method='first')
